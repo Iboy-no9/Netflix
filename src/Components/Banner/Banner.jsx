0de7aa81678d9d37d37042f0e1a1ffc4b4e86036
Muhammed Shadi <itsmeiboyno9@gmail.com>
2023-07-21 15:05:04 +0530

    Vedios Added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
-import './Banner.css'
+import React, { useEffect, useState } from 'react';
+import { API_KEY, imageUrl } from '../../Constants/constants';
+import axios from '../../axios';
+import './Banner.css';
+import { mockComponent } from 'react-dom/test-utils';
 
 function Banner() {
+  const [movie, setMovie] = useState();
+
+  useEffect(() => {
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response) => {
+      console.log(response.data);
+      setMovie(response.data.results[1]);
+    });
+  }, []);
+
   return (
-    <div className='banner'>
-        <div className='content'>
-            <h1 className='title'>Movie Name</h1>
-            <div className='banner_button'>
-                <button className='button'>Play</button>
-                <button className='button'>My list</button>
-            </div>
-            <h1 className='description'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the</h1>
+    <div style={{backgroundImage:`url(${movie? imageUrl+movie.backdrop_path :""})`}}
+    className='banner'>
+      <div className='content'>
+        <h1 className='title'>{movie? movie.title :''}</h1>
+        <div className='banner_button'>
+          <button className='button'>Play</button>
+          <button className='button'>My list</button>
         </div>
-        <div className='fade_bottom'></div>
+        <h1 className='description'>
+         {movie ? movie.overview :''}
+        </h1>
+      </div>
+      <div className='fade_bottom'></div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
---
+++ src/Constants/constants.jsx
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = 'a31dc682f04d9300e55eb4c523badcf9';
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
