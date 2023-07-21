d4d30b3466ca9cbc603259c49dd1d16b96190fd4
Muhammed Shadi <itsmeiboyno9@gmail.com>
2023-07-21 15:06:03 +0530

    Netflix Project Ended

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import './App.css';
+import {acitons,horror,orginals} from './urls'
 import Banner from './Components/Banner/Banner';
 import RowPoster from './Components/RowPoster/RowPoster';
 
@@ -8,9 +9,10 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Banner/>
-      {/* <RowPoster title='Netflix Originals'/>
-      <RowPoster title='Action' isSmall/> */}
+      {/* <Banner/>
+      <RowPoster url={orginals} title='Netflix Originals'/>
+      <RowPoster url={acitons} title='Action' isSmall/>
+      <RowPoster url={horror} title='Horror' isSmall/> */}
     </div>
   );
 }
